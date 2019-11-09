b1b3883470cf8c63fd9db6194bd25a9be541debc
Hemanth <hemanthsai10@gmail.com>
2019-11-08 19:11:17 -0500

    Socket bug

--- lib/timesheets_web/channels/sheet_socket.ex
+++ lib/timesheets_web/channels/sheet_socket.ex
@@ -5,25 +5,30 @@ defmodule Timesheets2Web.SheetChannel do
 
   def join("sheets:" <> name, payload, socket) do
       {user_id,_} = Integer.parse(name)
-      IO.inspect(socket)
       BackupAgent.put(user_id, socket)
       {:ok, %{"join" => name}, socket}
   end
 
   def broadcast_msg(%{"manager_id" => manager_id, "user_name" => user_name, "workdate" => workdate, "sheet_id" => sheet_id}) do
     socket = BackupAgent.get(manager_id)
-    message = "#{user_name} has submitted timesheet for #{workdate} with less than 8 hours"
-    Timesheets2Web.Endpoint.broadcast(socket.topic, "less_hours_alert" , %{sheet_id: sheet_id, message: message})
+    unless is_nil(socket) do
+      message = "#{user_name} has submitted timesheet for #{workdate} with less than 8 hours"
+      Timesheets2Web.Endpoint.broadcast(socket.topic, "less_hours_alert" , %{sheet_id: sheet_id, message: message})
+    end
   end
 
   def broadcast_sheet(%{"manager_id" => manager_id}) do
     socket = BackupAgent.get(manager_id)
-    Timesheets2Web.Endpoint.broadcast(socket.topic, "update_sheets" , %{})
+    unless is_nil(socket) do
+      Timesheets2Web.Endpoint.broadcast(socket.topic, "update_sheets" , %{})
+    end
   end
 
   def broadcast_approve(%{"worker_id" => worker_id}) do
     socket = BackupAgent.get(worker_id)
-    Timesheets2Web.Endpoint.broadcast(socket.topic, "update_sheets" , %{})
+    unless is_nil(socket) do
+      Timesheets2Web.Endpoint.broadcast(socket.topic, "update_sheets" , %{})
+    end
   end
 
 end
